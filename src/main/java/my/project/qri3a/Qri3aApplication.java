6118f8a47b795dc46262d5a45d74452765f80056
AbderrazzakNfissi <abderrazzak.nfissi@usmba.ac.ma>
2024-11-28 10:52:48 +0100

    intitial commit

---
+++ src/main/java/my/project/qri3a/Qri3aApplication.java
@@ -0,0 +1,13 @@
+package my.project.qri3a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qri3aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Qri3aApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/project/qri3a/Qri3aApplicationTests.java
@@ -0,0 +1,13 @@
+package my.project.qri3a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Qri3aApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
