05f4db5ef1aaf263302c89948fe9f4e6ca2a930e
Abderrazzak Nfissi <a.nfissi@henceforth.local>
2025-01-30 16:16:32 +0100

    added report respository

--- src/main/java/my/project/qri3a/repositories/ReportRepository.java
+++ src/main/java/my/project/qri3a/repositories/ReportRepository.java
@@ -1,4 +1,9 @@
 package my.project.qri3a.repositories;
 
-public interface ReportRepository {
+import my.project.qri3a.entities.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface ReportRepository extends JpaRepository<Report, UUID> {
+
 }
